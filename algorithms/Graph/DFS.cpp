bc3b23414fe4c5eb1ca3a88ff646fdb462f79ade
xbjiang <jxavierphysics@163.com>
2016-12-28 20:52:57 +0800

    create graph using adjacency matrix

--- algorithms/Graph/DFS.cpp
+++ algorithms/Graph/DFS.cpp
@@ -1,30 +1,63 @@
 #include "graph.h"
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
 bool visited[MAXVEX];
 
-void GFS(MGraph graph, int i)
+void DFS(MGraph* graph, int i)
 {
 	visited[i] = true;
-	printf("%c", graph.vexs[i]);
+	printf("%c", graph->vexs[i]);
 	int j;
-	for (j = 0; j < graph.numVertexes; j++)
+	for (j = 0; j < graph->numVertexes; j++)
 	{
-		if (graph.arc[i][j] == 1 && !visited[j])
-			GFS(graph, j);
+		if (graph->arc[i][j] == 1 && !visited[j])
+			DFS(graph, j);
 	}
 }
 
-void GFSTraverse(MGraph graph)
+void DFSTraverse(MGraph* graph)
 {
 	int i;
-	for (i = 0; i < graph.numVertexes; i++)
+	for (i = 0; i < graph->numVertexes; i++)
 	{
 		visited[i] = false;
 	}
 
-	for (i = 0; i < graph.numVertexes; i++)
+	for (i = 0; i < graph->numVertexes; i++)
 	{
 		if (!visited[i])
-			GFS(graph, i);
+			DFS(graph, i);
 	}
+}
+
+void createMGraph(MGraph* graph)
+{
+    int i, j, k, w;
+    cout << "Input the number of vertexes and edges: " << endl;
+    cin >> graph->numVertexes >> graph->numEdges;
+    cout << "Input the information of each vertex: " << endl;
+    
+    for (i = 0; i < graph->numVertexes; i++)
+        cin >> graph->vexs[i];
+    for (i = 0; i < graph->numVertexes; i++)
+        for (j = 0; j < graph->numVertexes; j++)
+            graph->arc[i][j] = 0;
+
+    for (k = 0; k < graph->numEdges; k++)
+    {
+        cout << "Input the index i, j and weight w of edge (vi, vj): " << endl;
+        cin >> i >> j >> w;
+        graph->arc[i][j] = w;
+        graph->arc[j][i] = w;
+    }
+}
+
+int main()
+{
+    MGraph graph;
+    createMGraph(&graph);
+    DFSTraverse(&graph);
+    cout << endl;
 }
